a48019d7959df5be81601eef4d07a706950f031e
Iyadb Horani <i3d@ironic3d.com>
2014-07-18 10:57:14 +0000

    Fixing the new comment agax render

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     @post = @topic.posts.find(params[:post_id])
 
     # @comment = @post.comments.build(params.require(:comment).permit(:body))
-    @comment =  Comment.new(params.require(:comment).permit(:body))
+    @comment =  Comment.new(comment_params)
     @comment.post = @post
     @new_comment = Comment.new
     @comment.user = current_user
@@ -33,18 +33,26 @@ class CommentsController < ApplicationController
 
     @comment = @post.comments.find(params[:id])
     authorize @comment
-
+    puts "*** START Destroy ***"
     if @comment.destroy
+      puts "*** Inside first IF Destroy ***"
       flash[:notice] = "Comment was removed"
       # redirect_to [@topic, @post]
     else
+      puts "*** Else Destroy ***"
       flash[:error] = "There was an error, please try again later"
       # render "posts/show"
       # redirect_to [@topic, @post]
     end
-
+    puts "*** Before Respond Destroy ***"
     respond_with(@comment) do |f|
       f.html { redirect_to [@topic, @post] }
     end
+  end
+  
+  private
+  
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
